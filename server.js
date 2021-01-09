2c0c20fa6901bd4d50a00bde6705f240755f2e3a
Nematjon <frank.sport.93@gmail.com>
2021-01-09 19:46:45 +0300

    Added some logic to /api/exercise/add route

--- server.js
+++ server.js
@@ -82,7 +82,27 @@ app.post("/api/exercise/add", function(req, res) {
     } else if(date && new Date(date).toString() === "Invalid Date") {
         res.json({"error": "Date should be yyyy-mm-dd format"})
     } else {
-        
+        usersCollection.findOneAndUpdate({_id: ObjectID(userId)},{$set: {
+          log: [{
+              description: description.trim(),
+              duration: +duration,
+              date: date ? new Date(date) : new Date()
+          }]
+        }}, (err, data) => {
+            if(err) {
+                console.log("Update error: ", err);
+                res.json({"error": "Update error"})
+            } else if(data) {
+                console.log(data, err)
+                res.json({
+                    username: data.value.username,
+                    description: data.value.log[data.value.log.length - 1].description,
+                    duration: data.value.log[data.value.log.length - 1].duration,
+                    _id: data.value._id,
+                    date: data.value.log[data.value.log.length - 1].date
+                })
+            }
+        } )
     }
 
 });
